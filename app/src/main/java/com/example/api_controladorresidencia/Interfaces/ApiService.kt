7fd81ac2f6b10a8fb31fa116b76d774b8c983eb2
unknown <achicandroid@gmail.com>
2025-05-12 19:57:24 -0500

    actualizacion 2

--- app/src/main/java/com/example/api_controladorresidencia/Interfaces/ApiService.kt
+++ app/src/main/java/com/example/api_controladorresidencia/Interfaces/ApiService.kt
@@ -7,11 +7,11 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 interface ApiService {
-    @GET("api/inquilino")
+    @GET("/api/inquilino")
     fun obtenerInquilinos(): Call<List<InquilinoM>>
 }
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:8081/"
+    private const val BASE_URL = "http://10.0.2.2:8081"
 
     val instancia: ApiService by lazy {
         Retrofit.Builder()
